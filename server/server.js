28d08f1c7520781205ac4a395ab170fb0e8d22ef
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-12-10 15:54:28 -0600

    added schemas folder

--- server/server.js
+++ server/server.js
@@ -26,7 +26,9 @@ const startApolloServer = async ( typeDefs, resolvers ) => {
   server.applyMiddleware({ app });
 
   db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    app.listen(PORT, () => 
+    console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
   });
 }
 

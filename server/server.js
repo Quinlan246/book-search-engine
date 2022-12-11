f3c4bd71accdd5f81509ae920736cbd68c5cec18
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-12-11 14:51:26 -0600

    added mutation to loginForm in Loginform.js

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const db = require('./config/connection');
 const routes = require('./routes');
 
 const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;

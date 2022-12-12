31173a014c613498449fa12a2bf3699bfc389303
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-12-11 18:29:42 -0600

    removed unused variables

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import {useMutation, useQuery} from '@apollo/react-hooks';
@@ -10,7 +9,7 @@ import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [ removeBook, { error } ] = useMutation(REMOVE_BOOK)
+  const [ removeBook ] = useMutation(REMOVE_BOOK)
 
   const userData = data?.me || [];
 
@@ -23,7 +22,7 @@ const SavedBooks = () => {
     }
 
     try {
-      const { data } = await removeBook({
+      await removeBook({
         variables: { bookId }
       })
       // upon success, remove book's id from localStorage
